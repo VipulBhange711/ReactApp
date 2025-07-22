056dcbf07984cd2f50a24658bd91fad62d77524a
VipulBhange711 <vipulbhange4164@gmail.com>
2025-07-22 20:51:38 +0530

    practice set-1 done

--- src/food.jsx
+++ src/food.jsx
@@ -5,9 +5,9 @@ function food(){
         const obj4 = 'Banana';
     return(
      <div>
-        
+
         <ul>
-            <li>{obj1}</li>
+            <li>{obj1.toUpperCase()}</li>
             <li>{obj2}</li>
             <li>{obj3}</li>
             <li>{obj4}</li>
