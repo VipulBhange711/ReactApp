fed0cc6f331bf2ef096f56ab05ad4b6692ba0aa9
VipulBhange711 <vipulbhange4164@gmail.com>
2025-07-27 03:51:15 +0530

    done onclick event

---
+++ src/onclickevent.jsx
@@ -0,0 +1,17 @@
+function onclickfunction(){
+
+    // const buttonclick = ()=> {console.log('hello world')};
+// const buttonclick = (e) =>{e.target.textContent = "Hello my world"; }; //ex-2
+
+
+    return(
+        <>
+
+        {/* <button onClick={buttonclick}> click on me</button>  */}
+        {/* <button onClick={buttonclick}> click on me</button> */} 
+        {/* ex-2 */}
+        </>
+    );
+
+}
+export default onclickfunction;
